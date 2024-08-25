45ef2d15098ca2813fd4fd82515dfa68fa574ab6
Aditya Aswani <42505138+aditya-aswani@users.noreply.github.com>
2024-08-25 17:50:07 -0400

    added alert and more js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const checkBtn = document.getElementById("check-btn");
+const input = document.getElementById("text-input");
 
 checkBtn.addEventListener("click", () => {
     const inputText = document.getElementById("text-input").value;
@@ -6,6 +7,20 @@ checkBtn.addEventListener("click", () => {
     checkIfPalindrome(inputText);
 });
 
+input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+
+        checkBtn.click();
+    }
+})
+
 const checkIfPalindrome = (text) => {
-    console.log(text);
+    if (text === "") {
+        alert("Please input a value.")
+    };
+
+    if (text.length === 1) {
+
+    }
 }
