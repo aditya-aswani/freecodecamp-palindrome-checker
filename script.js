42423f64cc94af4b79047f401daedac216cc6f08
Aditya Aswani <42505138+aditya-aswani@users.noreply.github.com>
2024-08-26 23:02:06 -0400

    added some more js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const checkBtn = document.getElementById("check-btn");
 const input = document.getElementById("text-input");
+const outputText = document.getElementById("result");
+const resultContainer = document.getElementById("result_container");
 
 checkBtn.addEventListener("click", () => {
     const inputText = document.getElementById("text-input").value;
@@ -16,11 +18,22 @@ input.addEventListener("keydown", (event) => {
 })
 
 const checkIfPalindrome = (text) => {
+    
+    resultContainer.classList.add("hidden-element");
+    outputText.innerText = "";
+
     if (text === "") {
         alert("Please input a value.")
     };
 
     if (text.length === 1) {
 
+        outputText.innerText = text;
+        resultContainer.classList.remove("hidden_element");
+
+    } else {
+        const cleanedString = text.replace(/[^a-zA-Z0-9]/g, "");
+
+        if 
     }
 }
